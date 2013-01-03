21ddef3dee76d265212bddd239c52c036903a332
Raphaël Davaillaud <rdavaillaud@hbs-research.com>
2013-01-03 15:55:20 +0100

    Backport changeset 33362  fixed a notice on PHP 5.4 (closes #9985, patch from bshaffer)

--- lib/widget/sfWidgetFormSelectCheckbox.class.php
+++ lib/widget/sfWidgetFormSelectCheckbox.class.php
@@ -104,7 +104,7 @@ class sfWidgetFormSelectCheckbox extends sfWidgetForm
         'id'    => $id = $this->generateId($name, self::escapeOnce($key)),
       );
 
-      if ((is_array($value) && in_array(strval($key), $value)) || strval($key) == strval($value))
+      if ((is_array($value) && in_array(strval($key), $value)) || (is_string($value) && strval($key) == strval($value)))
       {
         $baseAttributes['checked'] = 'checked';
       }
