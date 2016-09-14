7c98f06dcf0d0adfca43a748e5b5d2117c1737a3
Raphaël Davaillaud <rdavaillaud@hbs-research.com>
2016-09-14 16:18:40 +0200

    PHP7 remove statically called method

--- lib/plugins/sfPropelPlugin/lib/vendor/propel-generator/classes/propel/phing/AbstractPropelDataModelTask.php
+++ lib/plugins/sfPropelPlugin/lib/vendor/propel-generator/classes/propel/phing/AbstractPropelDataModelTask.php
@@ -439,7 +439,9 @@ abstract class AbstractPropelDataModelTask extends Task {
 						// normalize the document using normalizer stylesheet
 
 						$xsl = new XsltProcessor();
-						$xsl->importStyleSheet(DomDocument::load($this->xslFile->getAbsolutePath()));
+						$domDocument = new DomDocument();
+						$domDocument->load($this->xslFile->getAbsolutePath());
+						$xsl->importStyleSheet($domDocument);
 						$transformed = $xsl->transformToDoc($dom);
 						$newXmlFilename = substr($xmlFile->getName(), 0, strrpos($xmlFile->getName(), '.')) . '-transformed.xml';
 
